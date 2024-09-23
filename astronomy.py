3877bf71a134fe0234e1f6672e94263b7dfe78e6
Damian <damian.ivanov@icloud.com>
2024-09-23 02:13:44 -0400

    updated file names

--- astronomy.py
+++ astronomy.py
@@ -1,24 +1,16 @@
 from astropy import units as u
 from astropy import constants as const
+import values as val
 
-length = 26 * u.meter
+print(const.test)
 
-G = 6.67430e-11 * ((u.N*u.m**2)/u.kg**2)
-
-#astronomical data for various planets
-earth_vol_mean_radius_m = 6371000
-earth_mass_kg = 5.9722e24
-earth_period_yr = 1
-
-
-
-def force_gravity(m1, m2, r):
+#def force_gravity(m1, m2, r):
     #Universal law of gravitation
 
-    return((G*m1*m2)/(r**2))
+    #return((G*m1*m2)/(r**2))
 
-print(force_gravity(200,200,4))
+#print(force_gravity(200,200,4))
 
-print(G)
+#print(G)
 
-print(const.G)
+#print(const.G)
---
+++ values.py
@@ -0,0 +1,10 @@
+from astropy import units as u
+
+test = 5
+
+G = 6.67430e-11 * ((u.N*u.m**2)/u.kg**2)
+
+#astronomical data for various planets
+earth_vol_mean_radius_m = 6371000
+earth_mass_kg = 5.9722e24
+earth_period_yr = 1
